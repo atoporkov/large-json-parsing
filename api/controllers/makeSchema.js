53268f285660229227c84640a15fe075c8705a74
atoporkov <alex.toporkov90@gmail.com>
2017-07-21 15:40:44 +0300

    arrays parsing logic changed

--- api/controllers/makeSchema.js
+++ api/controllers/makeSchema.js
@@ -17,28 +17,43 @@ exports.makeSchema = function(req, res) {
         return result;
     }
 
-    let setNewSchema = (eventJson, schemaJson) => {
+    var setNewSchema = (eventJson, schemaJson) => {
 
-        let keys = {};
+        let keys = {},
+            _schemaJson = schemaJson || {};
 
-        Object.keys(eventJson).map((key) => {
-            if(!isExist(schemaJson, key) || Object.keys(schemaJson).length == 0)
-                keys[key] = new Date().getTime();
+        for(let key in eventJson){
+
+            if(!isExist(_schemaJson, key) || Object.keys(_schemaJson).length == 0){
+                if(!(eventJson instanceof Array)) {
+                    keys[key] = new Date().getTime();
+                }
+            }
+
+            //if(!(eventJson instanceof Array)) {
+            //    keys[key] = new Date().getTime();
+            //}
 
             if (typeof eventJson[key] === "object" && eventJson[key] !== null) {
-                let subKeys = setNewSchema(eventJson[key], schemaJson);
+                let subKeys = setNewSchema(eventJson[key], _schemaJson);
+
                 Object.keys(subKeys).map((subKey) => {
-                    subKeys[key + "." + subKey] = subKeys[subKey];
-                    delete subKeys[subKey];
+                    if(!(eventJson instanceof Array)){
+                        subKeys[key + "." + subKey] = subKeys[subKey];
+                        delete subKeys[subKey];
+                    }
                 });
+
                 Object.assign(keys, subKeys);
             }
-        });
+        }
 
         return keys;
     }
 
     let updateOldSchema = (newSchema, oldSchema) => {
+        if(!oldSchema) return newSchema;
+
         Object.keys(oldSchema).map((key) => {
             newSchema[key] = oldSchema[key];
         })
